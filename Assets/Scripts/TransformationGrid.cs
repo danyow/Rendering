3a5857f6c91a75c8857b578783ac037d1ae17d6c
Danyow <i.zuucol@gmail.com>
2019-02-12 20:19:58 +0800

    完成矩阵相关 PS：后续需要学习里面的矩阵算法和齐次坐标

--- Assets/Scripts/TransformationGrid.cs
+++ Assets/Scripts/TransformationGrid.cs
@@ -7,7 +7,8 @@ public class TransformationGrid : MonoBehaviour {
     public int gridResolution = 10;
     Transform[] grid;
     List<Transformation> transformations;
-    
+    Matrix4x4 transformation;
+
     void Awake() {
         grid = new Transform[gridResolution * gridResolution * gridResolution];
         for (int i = 0, z = 0; z < gridResolution; z++) {
@@ -20,12 +21,23 @@ public class TransformationGrid : MonoBehaviour {
         transformations = new List<Transformation>();
     }
     void Update() {
+        UpdateTransformation();
         GetComponents<Transformation>(transformations);
         for (int i = 0, z = 0; z < gridResolution; z++) {
             for (int y = 0; y < gridResolution; y++) {
                 for (int x = 0; x < gridResolution; x++, i++) {
                     grid[i].localPosition = TransformPoint(x, y, z);
                 }
+            }
+        }
+    }
+
+    void UpdateTransformation() {
+        GetComponents<Transformation>(transformations);
+        if (transformations.Count > 0) {
+            transformation = transformations[0].Matrix;
+            for (int i = 1; i < transformations.Count; i++) {
+                transformation = transformations[i].Matrix * transformation;
             }
         }
     }
@@ -55,10 +67,7 @@ public class TransformationGrid : MonoBehaviour {
     Vector3 TransformPoint (int x, int y, int z) {
         // 获取原有坐标后 然后应用到要转换的身上
 		Vector3 coordinates = GetCoordinates(x, y, z);
-		for (int i = 0; i < transformations.Count; i++) {
-			coordinates = transformations[i].Apply(coordinates);
-		}
-		return coordinates;
+		return transformation.MultiplyPoint(coordinates);
 	}
 
 }
